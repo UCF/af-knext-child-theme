93c94828788b66fafe0ec1f231c8c887b5797d10
Mike Setzer <michaelsetzerucf@gmail.com>
2023-04-03 17:02:35 -0400

    Added menu jquery

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,15 @@
 
 // Require your theme's custom script files here
 // ...
+$(document).ready(() => {
+  const currentUrl = window.location.href;
+
+  $('#wpt-menu a').each(function () {
+    const linkUrl = $(this).attr('href');
+
+    if (currentUrl === linkUrl) {
+      $(this).addClass('active text-secondary');
+      $(this).removeClass('text-inverse');
+    }
+  });
+});
