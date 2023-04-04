0ebcbb1db6d470eb270cc755f0a29dc7c0e8e8f6
Mike Setzer <michaelsetzerucf@gmail.com>
2023-04-04 15:30:14 -0400

    Namespace updates

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
-namespace MyProject\Theme;
+//namespace knext\Theme;
 
-define( 'MYPROJECT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'KNEXT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once MYPROJECT_THEME_DIR . 'includes/config.php';
-include_once MYPROJECT_THEME_DIR . 'includes/meta.php';
+include_once KNEXT_THEME_DIR . 'includes/config.php';
+include_once KNEXT_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
 /**
--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 /**
  * Handle all theme configuration here
  **/
-namespace MyProject\Theme\Includes\Config;
+//namespace knext\Theme\Includes\Config;
 
 
-define( 'MYPROJECT_THEME_URL', get_stylesheet_directory_uri() );
-define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
-define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
-define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
-define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+define( 'KNEXT_THEME_URL', get_stylesheet_directory_uri() );
+define( 'KNEXT_THEME_STATIC_URL', KNEXT_THEME_URL . '/static' );
+define( 'KNEXT_THEME_CSS_URL', KNEXT_THEME_STATIC_URL . '/css' );
+define( 'KNEXT_THEME_JS_URL', KNEXT_THEME_STATIC_URL . '/js' );
+define( 'KNEXT_THEME_IMG_URL', KNEXT_THEME_STATIC_URL . '/img' );
